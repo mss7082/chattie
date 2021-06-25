51caab17286fe52ea74075a600eb6110f79c3f84
Moses Sokabi <moses.sokabi@GMITESTMBP.mss>
2021-06-25 15:24:06 +0100

    added tokio select

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,44 @@
+use tokio::{
+    io::{AsyncBufReadExt, AsyncWriteExt, BufReader},
+    net::TcpListener,
+    sync::broadcast,
+};
+
 #[tokio::main]
 async fn main() {
-    println!("Hello");
+    let listener = TcpListener::bind("localhost:8080").await.unwrap();
+
+    let (tx, _rx) = broadcast::channel::<String>(10);
+
+    loop {
+        let (mut socket, _addr) = listener.accept().await.unwrap();
+
+        let tx = tx.clone();
+        let mut rx = tx.subscribe();
+
+        tokio::spawn(async move {
+            let (reader, mut writer) = socket.split();
+
+            let mut reader = BufReader::new(reader);
+
+            let mut line = String::new();
+            loop {
+                tokio::select! {
+
+
+                    result = reader.read_line(&mut line) => {
+                        if result.unwrap() == 0 {
+                            break;
+                        }
+                    tx.send(line.clone()).unwrap();
+                    line.clear();
+                    }
+                    result = rx.recv() => {
+                        let msg = result.unwrap();
+                        writer.write_all(msg.as_bytes()).await.unwrap();
+                    }
+                }
+            }
+        });
+    }
 }
